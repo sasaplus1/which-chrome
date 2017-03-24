0f11c4914c137d7634ad958221be00f1aaa3cd3c
sasaplus1 <sasaplus1@gmail.com>
2017-03-24 19:57:02 +0900

    fixed to cannot track path of Chrome in Ubuntu

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var karmaChromeLauncher = require('karma-chrome-launcher');
 var chromePaths = {};
 
 Object.keys(karmaChromeLauncher).forEach(function(key) {
-  var info, fullPath;
+  var info;
 
   if (key.indexOf('launcher:') !== 0) {
     return;
@@ -23,10 +23,7 @@ Object.keys(karmaChromeLauncher).forEach(function(key) {
     return;
   }
 
-  fullPath = info.DEFAULT_CMD[process.platform];
-
-  // NOTE: fs.existsSync is not deprecated in node.js 6.x or above
-  chromePaths[info.name] = fs.existsSync(fullPath) ? fullPath : null;
+  chromePaths[info.name] = info.DEFAULT_CMD[process.platform] || null;
 });
 
 module.exports = chromePaths;
